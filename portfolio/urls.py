0b43934e510fdc1f4d68efffa5d87cb52f001562
sanotresor123 <114482602+sanotresor123@users.noreply.github.com>
2022-11-23 11:22:55 +0200

    Delete portfolio directory

--- portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('signup/', views.signup_view, name="signup"),
-    path('delete/<int:id>', views.delete_v, name="delete"),
-]
